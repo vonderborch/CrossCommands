203820a2925749e5970a236f03af2b62e83f3900
Christian Webber <ricky.webber@hotmail.com>
2023-06-26 12:31:18 -0400

    testing

---
+++ test/manual/CrossCommands.Dev.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace CrossCommands.Dev.Test;
+
+class Program
+{
+    /// <summary>
+    /// Execute the program
+    /// </summary>
+    /// <param name="args">Command line arguments</param>
+    static void Main(string[] args)
+    {
+        Cross.Commands.RunCommand("", "git --version", createNewWindow: false, useShellExecute: false, redirectStandardError: true, redirectStandardOutput: true, errorOutputMethod: GitInstalledCheck, standardOutputMethod: GitInstalledCheck);
+    }
+
+    static void GitInstalledCheck(object sender, DataReceivedEventArgs e)
+    {
+        Console.WriteLine(e.Data);
+        if (true) ;
+    }
+}
+
